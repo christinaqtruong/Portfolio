b58250d25d53d02442e4e4c1016b27d4ada22b5c
Christina Truong <christinaqtruong@gmail.com>
2019-06-19 20:07:04 -0500

    edited about me paragraphs and added photo and styled margins

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,11 @@
 //Opens side bar navigation
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
 }
 
 //closes side bar navigation
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
 }
 
 //links
