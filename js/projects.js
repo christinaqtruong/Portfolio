a02277a02b0516025006bb92e399ac6ff2e3126e
Christina Truong <christinaqtruong@gmail.com>
2019-08-22 19:34:17 -0500

    changed resume name, added tags and developer process

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,19 @@
 var projects = [
   {
     title: "Engauge",
-    byline: "A data repository resource for oil companies",
+    dev: [
+      "Must have real life application and utility",
+      "Must use React.js",
+      "Must be backed by a MySQL or MongoDB Database with Sequelize or Mongoose ORM",
+      "Must be deployed using Heroku (with Data)",
+      "Must have both GET and POST routes",
+      "Must have a polished frontend / UI",
+      "Must utilize at least two libraries, packages or technologies",
+      "Must allow for or involve the authentication of users",
+      "Must have folder structure that meets MVC Paradigm",
+      "Must meet good quality coding standards (indentation, scoping, naming)",
+      "Must not expose sensitive API key information on the server"
+    ],
     demo: "../assets/gifs/engauge_demo.gif",
     description:
       "Engauge is a data repository customized for oil companies that stores, organizes and visualizes data as infographics and specialized tables. This app enables the creation of well tables, shows detailed information on each well and has a dashboard for quick access to summary statistics on oil, gas and water production. Engauge also displays the location of well sites and draws financial data from an API to populate the price of oil production based on current oil production.",
@@ -12,17 +24,36 @@ var projects = [
   },
   {
     title: "Heroes United X: The Awakening",
-    byline: "A JavaScript game",
+    dev: [
+      "Must use a Node and Express Web Server",
+      "Must be backed by a MySQL Database with a Sequelize ORM",
+      "Must have both GET and POST routes for retrieving and adding new data",
+      "Must be deployed using Heroku (with Data)",
+      "Must utilize at least one new library, package, or technology that we haven’t discussed",
+      "Must have a polished frontend / UI",
+      "Must have folder structure that meets MVC Paradigm",
+      "Must meet good quality coding standards (indentation, scoping, naming)",
+      "Must not expose sensitive API key information on the server"
+    ],
     demo: "../assets/gifs/heroes_united_demo.gif",
     description:
       "Heroes United is a multi-level, turn-based browser game that incorporates strategy, wit, and RPG elements. Users are prompted to login or sign up upon load, select and customize their characters, and unlock new chapters as they progress their way through the story. Characters have preset base stats according to their class, but the extra customization allows users to distribute additional stat points to any selected value.",
     link: "https://heroesunited.herokuapp.com",
-    tags: ["Express", "Node", "MySQL", "Passport", "Sequelize", "JavaScript", "jQuery", "D3"],
+    tags: [
+      "Express",
+      "Node",
+      "MySQL",
+      "Passport",
+      "Sequelize",
+      "JavaScript",
+      "jQuery",
+      "D3"
+    ],
     icon: "../assets/images/Heroes_United.png"
-  },
+  }
   // {
   //   title: "Lazy Timer",
-  //   byline: "An interval training stopwatch",
+  //   dev: "An interval training stopwatch",
   //   demo: "https://media.giphy.com/media/mP94uHyKvY1nq/giphy.gif",
   //   description:
   //     "The Lazy Timer allows the user to set two timers that switch back and forth automatically, enabling them to transition between workout and rest periods during interval training without having to reset the timer.",
@@ -32,7 +63,7 @@ var projects = [
   // },
   // {
   //   title: "Clicky Clicky 1000 Times Clicky",
-  //   byline: "A Rick & Morty Style Clicky Game",
+  //   dev: "A Rick & Morty Style Clicky Game",
   //   demo: "https://media.giphy.com/media/mP94uHyKvY1nq/giphy.gif",
   //   description:
   //     "This is everybody's favorite memory game, created using React and component states. It's Clicky Clicky 1000 Times Clicky! A game nobody should play but everyone should code at least once when getting introduced to React.js.",
@@ -44,17 +75,49 @@ var projects = [
 
 //for each item in the array, add a small icon to the projects divider
 
-function displayProject(){
+function displayProject() {
   //display first project on page
-  selectProject(projects[0].title, projects[0].byline, projects[0].description, projects[0].link, projects[0].demo);
+  selectProject(
+    projects[0].title,
+    projects[0].dev,
+    projects[0].description,
+    projects[0].link,
+    projects[0].demo
+  );
+
+  for (var j = 0; j < projects[0].tags.length; j++) {
+    var tagContainer = $("<div>")
+      .addClass("project-tag")
+      .attr("project-title", projects[i].title);
+
+    var tagDiv = $("<div class='project-tag'>").text(project[i].tags[j]);
+
+    $(tagContainer).append(tagDiv);
+
+    $(".tags").append(tagContainer);
+  }
+
+  for (var k = 0; k < projects[0].dev.length; k++) {
+    var devContainer = $("<ul>")
+      .addClass("project-dev")
+      .attr("project-title", projects[i].title);
+
+    var devDiv = $("<li class='project-dev'>").text(project[i].dev[k]);
+
+    $(devContainer).append(devDiv);
+
+    $(".dev-process").append(devContainer);
+  }
 
   //loop through projects and display their icons
-  for (var i=0; i < projects.length; i++){
-    var iconContainer = $("<div>").addClass("project-icon").attr({
-      "project-title": projects[i].title,
-      "data-key": i
-    });
-    
+  for (var i = 0; i < projects.length; i++) {
+    var iconContainer = $("<div>")
+      .addClass("project-icon")
+      .attr({
+        "project-title": projects[i].title,
+        "data-key": i
+      });
+
     var projectIcon = $("<img class='icon'>").attr("src", projects[i].icon);
 
     iconContainer.append(projectIcon);
@@ -63,41 +126,63 @@ function displayProject(){
   }
 
   //switch between projects
-  $(".project-list").on("click", ".project-icon", function(){
-    for (var i=0; i < projects.length; i++){
-      if($(this).attr("project-title") === projects[i].title){
-        selectProject(projects[i].title, projects[i].byline, projects[i].description, projects[i].link, projects[i].demo)
+  $(".project-list").on("click", ".project-icon", function() {
+    for (var i = 0; i < projects.length; i++) {
+      if ($(this).attr("project-title") === projects[i].title) {
+        selectProject(
+          projects[i].title,
+          projects[i].description,
+          projects[i].link,
+          projects[i].demo
+        );
+
+        //append tags based on project selected
+        for (var j = 0; j < projects[i].tags.length; j++) {
+          var tagContainer = $("<div>")
+            .addClass("project-tag")
+            .attr("project-title", projects[i].title);
+
+          var tagDiv = $("<div class='project-tag'>").text(project[i].tags[j]);
+
+          $(tagContainer).append(tagDiv);
+
+          $(".tags").append(tagContainer);
+        }
+
+        //append list of development process based on project selected
+        for (var k = 0; k < projects[i].dev.length; k++) {
+          var devContainer = $("<ul>")
+            .addClass("project-dev")
+            .attr("project-title", projects[i].title);
+
+          var devDiv = $("<li class='project-dev'>").text(project[i].dev[k]);
+
+          $(devContainer).append(devDiv);
+
+          $(".dev-process").append(devContainer);
+        }
       }
     }
-  })
+  });
 }
 
-function selectProject(title, byline, description, link, demo){
+function selectProject(title, dev, description, link, demo) {
   //clear previous selection
   $(".title").empty();
-  $(".byline").empty();
+  $(".dev").empty();
   $(".description").empty();
   $(".demo-link").empty();
   $(".demo-gif").empty();
+  $(".tags").empty();
 
   //display project details
   $(".title").text(title);
-  $(".byline").text(byline);
   $(".description").text(description);
   $(".demo-link").text(title);
   $(".demo-link").attr("href", link);
   $(".demo-gif").attr("src", demo);
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
   displayProject();
-})
-
-// var column = "<div class='col-sm slide'></div>";
-
-// $.each(projects, function(index, val) {
-//   var col = $(column);
-//   var img = $("<img>").attr("src", val.icon);
-//   col.append(img);
-//   $("#project-bar").append(col);
-// });
+});
