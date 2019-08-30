562ea81543a5d9b5e05a490d4944f53edb7ab84b
Christina Truong <christinaqtruong@gmail.com>
2019-08-30 12:39:43 -0500

    filled in information for the rest of the projects

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ var projects = [
   {
     title: "Engauge",
     dev: [
-      "User logs in to access app information via user authentication",
+      "User logs in to access app information via Passport.js",
       "User lands on a dashboard containing charts and graphs populated using Nivo that summarizes user data",
       "Pages are populated using React.js, with specific information dynamically populated using states",
       "Data is pushed into/pulled from MongoDB using both GET and POST routes and Mongoose ORM",
@@ -12,7 +12,7 @@ var projects = [
     background:
       "This project was a collaborative effort with teammates <a href='https://github.com/elaintran'>Elain Tran</a>, <a href='https://github.com/i-k0n/'>Mark Baker</a>, <a href='https://github.com/Dgarza0413'>David Garza</a>, and <a href='https://github.com/bmm019'>Brittany McNulty</a>. David wanted to create an app that would have helped him in his former role as a geologist recording oil well data in the field. Under his direction, we created Enguage, which is targeted at small oil companies for tracking data.",
     description:
-      "Engauge organizes data users input through forms into a mongoose database and accesses the information to dynamically create charts, maps and financial data using an API. My role involved utilizing get/post routes to dynamically create populate data in tables retrieved from a Mongo database, creating table components using React.js and building search/filter functions for better user experience and organization.",
+      "Engauge organizes data users input through forms into a mongoose database and accesses the information to dynamically create charts, maps and financial data using an API. My role involved utilizing get/post routes to dynamically create populate data in tables, creating table components using React.js and building search/filter functions for better user experience and organization.",
     link: "https://serene-meadow-56536.herokuapp.com/",
     tags: ["React", "MongoDB", "Mongoose", "Passport", "Moment", "JavaScript", "Nivo", "Google Map React"],
     icon: "../assets/images/Engauge.png"
@@ -20,19 +20,21 @@ var projects = [
   {
     title: "Heroes United X: The Awakening",
     dev: [
-      "Must use a Node and Express Web Server",
-      "Must be backed by a MySQL Database with a Sequelize ORM",
-      "Must have both GET and POST routes for retrieving and adding new data",
-      "Must be deployed using Heroku (with Data)",
-      "Must utilize at least one new library, package, or technology that we haven’t discussed",
-      "Must have a polished frontend / UI",
-      "Must have folder structure that meets MVC Paradigm",
-      "Must meet good quality coding standards (indentation, scoping, naming)",
-      "Must not expose sensitive API key information on the server"
+      "User must create a username and password stored in a MySQL Database to log in and create a character",
+      "User authentication via Passport.js allows previous users to relog and access stored character and chapter progress data",
+      "A Node and Express Web Server is used to navgiate different stages of the game",
+      "GET and POST routes retrieve and add new data to the database as chapters are completed or character stats changed",
+      "Illustrator used to create svgs for head, body and leg attack selections",
+      "Animations done using JavaScript and jQuery",
+      "Character sprites grabbed from Nintendo's Fire Emblem game",
+      "Character health bar display changes dynamically using D3.js",
+      "Combat system incorporates elements of character speed, attack, defense and hit chance, enacted using JavaScript"
     ],
     demo: "../assets/gifs/heroes_united_demo.gif",
+    background:
+      "This was the first collaborative effort with my amazing teammates <a href='https://github.com/elaintran'>Elain Tran</a>, <a href='https://github.com/i-k0n/'>Mark Baker</a>, <a href='https://github.com/Dgarza0413'>David Garza</a>, and <a href='https://github.com/bmm019'>Brittany McNulty</a>. We all thought it would be cool to create a game with a combat system for our first big project incorporating backend data storage and came up with Heroes.",
     description:
-      "Collaborative multi-level, turn-based browser game that incorporates strategy and RPG elements. Utilizes user authentication to create and save characters, jQuery for gameplay and MySQL to store character statistics and game progress. Created the game engine and combat system.",
+      "Heroes is a collaborative multi-level, turn-based browser game that incorporates strategy and RPG elements. User authentication is used to access saved character statistics/game progression, jQuery for gameplay and a MySQL database for data storage. My role involved designing the game play, coding the combat system, and working with the front-end developers to tie in the game mechanics with the UI.",
     link: "https://heroesunited.herokuapp.com",
     tags: [
       "Express",
@@ -48,10 +50,18 @@ var projects = [
   },
   {
     title: "Lazy Timer",
-    dev: "",
+    dev: 
+      ["An input form is dynmically populated upon clicking the timer display",
+      "Upon submit, the data is pushed to Firebase and reflected real time",
+      "The values are checked for certain parameters and converted a common unit using regex for entries separated by semicolons",
+      "The values are converted back to time format for display using Moment.js",
+      "setInterval() is used for the timers, with Booleans set to determine which timer is running at any given time"
+      ],
     demo: "../assets/gifs/lazy_timer_demo.gif",
+    background:
+      "I prefer creating apps that have real-life application, so when the time came to create my first app, I decided to look to my past experiences to see what would've helped me. When I moved back to Austin and started my first night shift job at a hospital, it completely messed up my daily routine, affecting my sleeping and active hours. In an attempt to keep in shape, I picked up interval training and calisthenics as a way to relieve stress and keep busy during my downtime. The need for two timers for my interval training inspired me to create this app.",
     description:
-      "An interval training stopwatch that allows the user to set two timers that switch back and forth automatically, enabling them to transition between workout and rest periods during interval training without having to reset the timer.",
+      "Lazy Timer is an interval training stopwatch that allows the user to set two timers that switch back and forth automatically, enabling them to transition between workout and rest periods during interval training without having to reset the timer.",
     link: "https://christinaqtruong.github.io/LazyTimer/",
     tags: ["JavaScript", "jQuery", "CSS"],
     icon: "../assets/images/Lazy_Timer.png"
